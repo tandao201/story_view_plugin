f58de8f0a2a4f71fb09033ebf9c01898a258babb
tandc <tan.c.dao@vntrip.vn>
2023-04-04 14:46:49 +0700

    fix bug

--- lib/widgets/story_view.dart
+++ lib/widgets/story_view.dart
@@ -46,7 +46,7 @@ class StoryItem {
     this.music = "",
     this.createdTime = "",
     this.id = -1,
-    this.usersView = [],
+    this.usersView = <dynamic>[],
   });
 
   /// Short hand to create text-only page.
